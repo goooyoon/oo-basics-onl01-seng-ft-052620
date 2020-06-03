eca28d4c6e31186c3060490db1f72a92ccb367fc
Grace Kim <goooyoon@gmail.com>
2020-06-03 00:19:22 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,4 @@
+ 
 class Book
  
   def initialize(title)
@@ -11,9 +12,17 @@ class Book
   def author=(author)
     @author = author
   end
-  
+ 
   def author
     @author
+  end
+ 
+  def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
   end
  
 end
